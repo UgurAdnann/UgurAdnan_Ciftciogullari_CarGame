5bf6c0c8a10bcda101c7a09e875267ae8f36c7ff
UgurAdnann <adnann353@gmail.com>
2023-07-21 19:36:36 +0300

    Commit

--- Assets/Scripts/Player/PlayerClass.cs
+++ Assets/Scripts/Player/PlayerClass.cs
@@ -32,7 +32,7 @@ public class PlayerClass : MonoBehaviour
 
     public void FollowPath()
     {
-        transform.DOPath(pathPoints, 7.5f, PathType.Linear, PathMode.Full3D, 10 /* resolution*/, Color.red);
+        transform.DOPath(pathPoints, 7.5f, PathType.Linear, PathMode.Full3D, 10 /* resolution*/, Color.red).SetEase(Ease.Linear);
     }
 
     private IEnumerator WaitCreatePath()
